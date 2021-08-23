2dd637346c02f53f6f382ff03668eb24ae4861fa
DavidBarringerQA <DBarringer@qa.com>
2021-08-23 14:14:22 +0100

    DTO fixing

--- src/main/java/com/qa/choonz/rest/dto/GenreDTO.java
+++ src/main/java/com/qa/choonz/rest/dto/GenreDTO.java
@@ -1,86 +1,91 @@
 package com.qa.choonz.rest.dto;
-
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
-
 import com.qa.choonz.persistence.domain.Album;
-
 public class GenreDTO {
 
-    private long id;
-    private String name;
-    private String description;
-    private List<Album> albums;
+	private long id;
+	private String name;
+	private String description;
+	private Map<Long, String> albums;
 
-    public GenreDTO() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	public GenreDTO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
-    public GenreDTO(long id, String name, String description, List<Album> albums) {
-        super();
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.albums = albums;
-    }
+	public GenreDTO(long id, String name, String description, List<Album> albums) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.albums = new HashMap<>();
+		for(Album album : albums){
+			this.albums.put(album.getId(), album.getName());
+		}
+	}
 
-    public long getId() {
-        return id;
-    }
+	public long getId() {
+		return id;
+	}
 
-    public void setId(long id) {
-        this.id = id;
-    }
+	public void setId(long id) {
+		this.id = id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public String getDescription() {
-        return description;
-    }
+	public String getDescription() {
+		return description;
+	}
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-    public List<Album> getAlbums() {
-        return albums;
-    }
+	public Map<Long,String> getAlbums() {
+		return albums;
+	}
 
-    public void setAlbums(List<Album> albums) {
-        this.albums = albums;
-    }
+	public void setAlbums(List<Album> albums) {
+		this.albums = new HashMap<>();
+		for(Album album : albums){
+			this.albums.put(album.getId(), album.getName());
+		}
+	}
 
-    @Override
-    public String toString() {
-        StringBuilder builder = new StringBuilder();
-        builder.append("GenreDTO [id=").append(id).append(", name=").append(name).append(", description=")
-                .append(description).append(", albums=").append(albums).append("]");
-        return builder.toString();
-    }
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("GenreDTO [id=").append(id).append(", name=").append(name).append(", description=")
+			.append(description).append(", albums=").append(albums).append("]");
+		return builder.toString();
+	}
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(albums, description, id, name);
-    }
+	@Override
+	public int hashCode() {
+		return Objects.hash(albums, description, id, name);
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (!(obj instanceof GenreDTO)) {
-            return false;
-        }
-        GenreDTO other = (GenreDTO) obj;
-        return Objects.equals(albums, other.albums) && Objects.equals(description, other.description) && id == other.id
-                && Objects.equals(name, other.name);
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof GenreDTO)) {
+			return false;
+		}
+		GenreDTO other = (GenreDTO) obj;
+		return Objects.equals(albums, other.albums) && Objects.equals(description, other.description) && id == other.id
+			&& Objects.equals(name, other.name);
+	}
 
 }
--- src/main/java/com/qa/choonz/rest/dto/TrackDTO.java
+++ src/main/java/com/qa/choonz/rest/dto/TrackDTO.java
@@ -1,20 +1,112 @@
 package com.qa.choonz.rest.dto;
 
+import java.util.HashMap;
+import java.util.Objects;
 import com.qa.choonz.persistence.domain.Album;
 import com.qa.choonz.persistence.domain.Playlist;
 
 public class TrackDTO {
 
-    private long id;
-    private String name;
-    private Album album;
-    private Playlist playlist;
-    private int duration;
-    private String lyrics;
+	private long id;
+	private String name;
+	private HashMap<Long, String> album;
+	private HashMap<Long, String> playlist;
+	private int duration;
+	private String lyrics;
 
-    public TrackDTO() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	public TrackDTO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public TrackDTO(long id, String name, Album album, Playlist playlist, int duration, String lyrics){
+		super();
+		this.id = id;
+		this.name = name;
+		this.album = new HashMap<>();
+		this.album.put(album.getId(), album.getName());
+		this.playlist = new HashMap<>();
+		this.playlist.put(playlist.getId(), playlist.getName());
+		this.duration = duration;
+		this.lyrics = lyrics;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public HashMap<Long, String> getAlbum() {
+		return album;
+	}
+
+	public void setAlbum(Album album) {
+		this.album = new HashMap<>();
+		this.album.put(album.getId(), album.getName());
+	}
+
+	public HashMap<Long, String> getPlaylist() {
+		return playlist;
+	}
+
+	public void setPlaylist(Playlist playlist) {
+		this.playlist = new HashMap<>();
+		this.playlist.put(playlist.getId(), playlist.getName());
+	}
+
+	public int getDuration() {
+		return duration;
+	}
+
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+
+	public String getLyrics() {
+		return lyrics;
+	}
+
+	public void setLyrics(String lyrics) {
+		this.lyrics = lyrics;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("TrackDTO [id=").append(id).append(", name=").append(name).append(", album=").append(album)
+			.append(", playlist=").append(playlist).append(", duration=").append(duration).append(", lyrics=")
+			.append(lyrics).append("]");
+		return builder.toString();
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name, album, playlist, duration, lyrics);
+	}
+
+	@Override
+	public boolean equals(Object obj){
+		if (this == obj){
+			return true;
+		}
+		if(!(obj instanceof TrackDTO)){
+			return false;
+		}
+		TrackDTO other = (TrackDTO) obj;
+		return id == other.id && Objects.equals(name, other.name) && Objects.equals(album, other.album)
+			&& Objects.equals(playlist, other.playlist) && duration == other.duration &&
+			Objects.equals(lyrics, other.lyrics);
+	}
 
 }
