992e05e5425f96c9c875ba1452cc19ead0894099
simonwhiteQA <simonwhite17@icloud.com>
2021-08-25 17:32:19 +0100

    FP2-39 #comment initial commit for tracks CRUD javascript

---
+++ src/main/resources/static/js/tracks/trackDelete.js
@@ -0,0 +1,27 @@
+let delReq = () => {
+    let nameEntry = document.querySelector("#delete-name").value;
+    let trackTable = document.querySelector("#track-table");
+    let id = 0;
+    console.log(nameEntry);
+    let tracks = trackTable.querySelectorAll(".tracks");
+    for (t of tracks) {
+        console.log(t.textContent);
+    if (nameEntry == t.textContent) {
+        id = t.getAttribute("id");
+    }
+}
+    fetch("http://localhost:8082/tracks/delete/" + id, {
+        method: 'DELETE'
+    }).then((data) => {
+        if (data.status === 204) {
+            console.log("delete success");
+        }
+    }).catch((error) => {
+        console.log("Oops..." + error);
+    });
+    let modal = document.getElementById("close-modal");
+    modal.click();
+}
+
+var submit = document.getElementById("delete-btn");
+submit.addEventListener("click", delReq);
