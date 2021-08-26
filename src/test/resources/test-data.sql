5525c067010cab0f2970a494215a49b7f896f425
DavidBarringerQA <DBarringer@qa.com>
2021-08-26 17:08:37 +0100

    FP2-21 #comment Added track integration tests

--- src/test/resources/test-data.sql
+++ src/test/resources/test-data.sql
@@ -2,3 +2,4 @@ INSERT INTO `genre` (`name`, `description`) VALUES ('TestGenre', 'TestDescriptio
 INSERT INTO `artist` (`name`) VALUES ('TestArtist');
 INSERT INTO `playlist` (`name`, `description`, `artwork`) VALUES ('TestPlaylist', 'TestDescription', 'TestArtwork');
 INSERT INTO `album` (`name`, `artist_id`, `genre_id`, `cover`) VALUES ('TestAlbum', 1, 1, 'TestCover');
+INSERT INTO `track` (`name`, `duration`, `lyrics`, `album_id`, `playlist_id`) VALUES ('TestTrack', 100, 'TestLyrics', 1, 1);
